19b66a0a744078d9fea6780caac98d594d088833
Masood Masaeli <masood@ngti.nl>
2021-01-24 02:29:28 +0100

    Akka actor creation and find

--- src/main/scala/org/masood/messenger/telegram/bot/App.scala
+++ src/main/scala/org/masood/messenger/telegram/bot/App.scala
@@ -3,33 +3,23 @@ package org.masood.messenger.telegram.bot
 import akka.actor.{ActorSystem, PoisonPill, Props}
 import com.bot4s.telegram.models.User
 import org.masood.actor.GameActor
-import org.masood.messenger.telegram.bot.{MafiaBot => rb}
 
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
 
 object App {
   def main(args: Array[String]): Unit = {
-    val system = ActorSystem("mafia")
-    val counter1 = system.actorOf(Props(new GameActor(new User(0, false,"Zeus"))), "Game")
-    println(s"actor reference for $counter1")
-    val actorSelection1 = system.actorSelection("counter")
-    println(s"actor selection for $actorSelection1")
-    counter1 ! PoisonPill
-    Thread.sleep(100)
-    system.terminate()
-//    runBot()
+    runBot()
   }
 
-//  def runBot() = {
-//    // To run spawn the bot
-//    val bot = new rb("TOKEN")
-//    val eol = bot.run()
-//    println("Press [ENTER] to shutdown the bot, it may take a few seconds...")
-//    scala.io.StdIn.readLine()
-//    bot.shutdown() // initiate shutdown
-//    // Wait for the bot end-of-life
-//    Await.result(eol, Duration.Inf)
-//  }
+  def runBot(): Unit = {
+    // To run spawn the bot
+    val bot = new MafiaBot(sys.env("TELEGRAM_TOKEN"))
+    val eol = bot.run()
+    println("Press [ENTER] to shutdown the bot, it may take a few seconds...")
+    scala.io.StdIn.readLine()
+    bot.shutdown()
+    Await.result(eol, Duration.Inf)
+  }
 }
 
--- src/main/scala/org/masood/messenger/telegram/bot/MafiaBot.scala
+++ src/main/scala/org/masood/messenger/telegram/bot/MafiaBot.scala
@@ -1,15 +1,19 @@
 package org.masood.messenger.telegram.bot
 
+import akka.actor.{ActorSystem, Props}
 import cats.instances.future._
 import cats.syntax.functor._
 import com.bot4s.telegram.api.RequestHandler
 import com.bot4s.telegram.api.declarative.Commands
-import com.bot4s.telegram.clients.{FutureSttpClient, ScalajHttpClient}
+import com.bot4s.telegram.clients.ScalajHttpClient
 import com.bot4s.telegram.future.{Polling, TelegramBot}
+import org.apache.commons.lang.RandomStringUtils
+import org.masood.actor.GameActor
 import slogging.{LogLevel, LoggerConfig, PrintLoggerFactory}
 
+import scala.concurrent.duration.{Duration, MILLISECONDS}
+import scala.concurrent.{Await, Future}
 import scala.util.Try
-import scala.concurrent.Future
 
 /** Generates random values.
  */
@@ -21,20 +25,40 @@ class MafiaBot(val token: String) extends TelegramBot
   // set log level, e.g. to TRACE
   LoggerConfig.level = LogLevel.TRACE
 
+  private val ctx: ActorSystem = ActorSystem("mafia")
+
   // Or just the scalaj-http backend
   override val client: RequestHandler[Future] = new ScalajHttpClient(token)
-
   onCommand("help" or "h") { implicit msg =>
     reply(
       s"""/help, /h: prints this message.
          |/new: starts a new game
+         |/join, /j game_id: Join to a game
          |/report, /r [verbose, v]: prints game report. Use optional parameter verbose (or v) to get a detailed report for every night.
          |""".stripMargin).void
   }
 
   onCommand("new") { implicit msg =>
-    withArgs( args =>
-      reply(s"$msg  0000000 $args").void
+    val random: String = RandomStringUtils.randomNumeric(6)
+    ctx.actorOf(Props(new GameActor(msg.from.get)), s"actor-$random")
+    reply(s"A new game has been initialized. ID: '$random'").void
+  }
+
+  onCommand("join" | "j") { implicit msg =>
+    withArgs(args =>
+      if (args.size != 1) {
+        reply("Give me valid game id to join").void
+      } else {
+        val actorSelection = ctx.actorSelection(s"/user/actor-${args.head}/")
+        val resolved = actorSelection.resolveOne(Duration(700, MILLISECONDS))
+        val ready = Await.ready(resolved, Duration(700, MILLISECONDS))
+
+        if (ready.value.isDefined && ready.value.get.isSuccess) {
+          reply(s"${ready.value.get.get}").void
+        } else {
+          reply(s"'${args.head}' is not a valid game id").void
+        }
+      }
     )
   }
 
@@ -42,4 +66,5 @@ class MafiaBot(val token: String) extends TelegramBot
   object Int {
     def unapply(s: String): Option[Int] = Try(s.toInt).toOption
   }
+
 }
